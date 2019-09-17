b8240d62130d34b6619cb5ead753e26b136d8f1d
Emma Casey <emma.casey@cantab.net>
2019-09-17 14:39:30 +0100

    adding password testing and generation

--- api/client.py
+++ api/client.py
@@ -1,5 +1,57 @@
-import requests
+import requests # We're using a library to simplify making API requests
+
+
 print("1+1=")
+#send a post request to our server to do this terribly complicated calculation for us
 r = requests.get("http://127.0.0.1:5000/compute/sum?parma=1&parmb=1")
+# Throw an error if the get request didn't work
+assert r.status_code == 200
+print(r.text)
+print()
+
+print("Is your password secure?")
+password=input("Enter your password:")
+r = requests.get(f"http://127.0.0.1:5000/evaluate?password={password}")
+assert r.status_code==200
+print(r.text)
+
+print("How about we make a new one?")
+config={}
+config['length']=int(input("How many words?"))
+config['separator']=input("What should go between words? ('-','_','+')")
+while True:
+    response=input("Special characters? (y/n)")
+    if response=="y":
+        config['special']=True
+        break
+    elif response=="n":
+        config['special']=False
+        break
+    else:
+        print("Please type 'y' or 'n'")
+
+while True:
+    response=input("Include a number? (y/n)")
+    if response=="y":
+        config['number']=True
+        break
+    elif response=="n":
+        config['number']=False
+        break
+    else:
+        print("Please type 'y' or 'n'")
+
+while True:
+    response=input("Make the first letter of each word uppercase? (y/n)")
+    if response=="y":
+        config['uppercase']=True
+        break
+    elif response=="n":
+        config['uppercase']=False
+        break
+    else:
+        print("Please type 'y' or 'n'")
+
+r=requests.post("http://127.0.0.1:5000/generate",json=config)
 assert r.status_code == 200
 print(r.text)
--- api/server.py
+++ api/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_restplus import Api, Resource, fields
+import random
+import string
 
 app = Flask(__name__)
 
@@ -27,6 +29,49 @@ class Compute(Resource):
             return a/b
         else:
             return f"sorry, I don't know how to '{action}'"
+
+@api.route('/evaluate')
+@api.doc(params={"password":"hunter2"})
+class Sum(Resource):
+    def get(self):
+        return "Of course not, you sent it as plaintext over an unsecured connection. You muppet."
+
+def random_word(uppercase=False):
+    with open("words.txt") as f:
+        raw_words=f.read()
+    split_words=raw_words.split()
+    total_words=len(split_words)
+    random_index=random.randint(0,total_words)
+    word=split_words[random_index]
+    if uppercase:
+        word=word[0].upper()+word[1:]
+    return word
+
+post_fields = api.model('Model', {
+         'length': fields.Integer(default=4),
+         "special": fields.Boolean,
+         "number": fields.Boolean,
+         "separator": fields.String(default=""),
+         "uppercase": fields.Boolean,
+             })
+@api.route('/generate')
+class generatePassword(Resource):
+
+     @api.expect(post_fields)
+     def post(self):
+         json_data = request.json
+         password=""
+         for i in range(json_data['length']):
+             if password is not "":
+                 password+=json_data['separator']
+             uppercase = json_data['uppercase']
+             password+=random_word(uppercase)
+         if json_data['special']:
+             password+=random.choice(string.punctuation)
+         if json_data['number']:
+             password+=random.choice(string.digits)
+         return password
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
