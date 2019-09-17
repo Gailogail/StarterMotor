35dc50761e569de8c2f42ba5a38d938d16135fc5
Emma Casey <emma.casey@cantab.net>
2019-09-17 14:58:14 +0100

    formatting and documentation

--- api/README.md
+++ api/README.md
@@ -5,3 +5,28 @@ To run:
 * Run server with `python server.py`
 * Check 127.0.0.1:5000 in your browser to see if it's running correctly
 * Run client with `python client.py`
+
+The server offers 4 APIs:
+* GET /compute/<action>?first=a&second=b
+    * if <action> is "sum" return a+b
+    * if <action> is "multiply" return axb
+    * if <action> is "divide" return a/b
+    * if <action> is "subtract" return a-b
+* GET /sum?first=a&second=b
+* GET /evaluate?password=hunter2
+        * tells the user they are an idiot
+* POST /generate
+        * takes a json of the form:
+                {
+                  "length": 4,
+                  "special": true,
+                  "number": true,
+                  "separator": "-",
+                  "uppercase": true
+                }
+        * returns a correct-horse-battery-staple password
+
+The client goes through 3 steps:
+* A basic GET request to /compute/
+* Asks the user for their password, and then returns the response of /evaluate
+* Asks for some configuration settings and then returns a Correct Horse Battery Stabple password
--- api/server.py
+++ api/server.py
@@ -5,72 +5,86 @@ import string
 
 app = Flask(__name__)
 
-api = Api(app, version='1.0', title="My First API", description="Here's an API to try, there are several methods here you can ping")
+api = Api(
+    app,
+    version="1.0",
+    title="My First API",
+    description="Here's an API to try, there are several methods here you can ping",
+)
 
-@api.route('/sum')
-@api.doc(params={"parma":"2","parmb":"3"})
+
+@api.route("/sum")
+@api.doc(params={"first": "2", "second": "3"})
 class Sum(Resource):
     def get(self):
-        return int(request.args.get("parma"))+int(request.args.get("parmb"))
+        return int(request.args.get("first")) + int(request.args.get("second"))
 
-@api.route('/compute/<string:action>')
-@api.doc(params={"parma":"2","parmb":"3"})
+
+@api.route("/compute/<string:action>")
+@api.doc(params={"first": "2", "second": "3"})
 class Compute(Resource):
-    def get(self,action):
-        a=int(request.args.get("parma"))
-        b=int(request.args.get("parmb"))
-        if action =="sum":
-            return a+b
+    def get(self, action):
+        a = int(request.args.get("first"))
+        b = int(request.args.get("second"))
+        if action == "sum":
+            return a + b
         elif action == "subtract":
-            return a-b
+            return a - b
         elif action == "multiply":
-            return a*b
+            return a * b
         elif action == "divide":
-            return a/b
+            return a / b
         else:
             return f"sorry, I don't know how to '{action}'"
 
-@api.route('/evaluate')
-@api.doc(params={"password":"hunter2"})
+
+@api.route("/evaluate")
+@api.doc(params={"password": "hunter2"})
 class Sum(Resource):
     def get(self):
         return "Of course not, you sent it as plaintext over an unsecured connection. You muppet."
 
+
 def random_word(uppercase=False):
     with open("words.txt") as f:
-        raw_words=f.read()
-    split_words=raw_words.split()
-    total_words=len(split_words)
-    random_index=random.randint(0,total_words)
-    word=split_words[random_index]
+        raw_words = f.read()
+    split_words = raw_words.split()
+    total_words = len(split_words)
+    random_index = random.randint(0, total_words)
+    word = split_words[random_index]
     if uppercase:
-        word=word[0].upper()+word[1:]
+        word = word[0].upper() + word[1:]
     return word
 
-post_fields = api.model('Model', {
-         'length': fields.Integer(default=4),
-         "special": fields.Boolean,
-         "number": fields.Boolean,
-         "separator": fields.String(default=""),
-         "uppercase": fields.Boolean,
-             })
-@api.route('/generate')
-class generatePassword(Resource):
 
-     @api.expect(post_fields)
-     def post(self):
-         json_data = request.json
-         password=""
-         for i in range(json_data['length']):
-             if password is not "":
-                 password+=json_data['separator']
-             uppercase = json_data['uppercase']
-             password+=random_word(uppercase)
-         if json_data['special']:
-             password+=random.choice(string.punctuation)
-         if json_data['number']:
-             password+=random.choice(string.digits)
-         return password
+post_fields = api.model(
+    "Model",
+    {
+        "length": fields.Integer(default=4),
+        "special": fields.Boolean,
+        "number": fields.Boolean,
+        "separator": fields.String(default=""),
+        "uppercase": fields.Boolean,
+    },
+)
+
+
+@api.route("/generate")
+class generatePassword(Resource):
+    @api.expect(post_fields)
+    def post(self):
+        json_data = request.json
+        password = ""
+        for i in range(json_data["length"]):
+            if password is not "":
+                password += json_data["separator"]
+            uppercase = json_data["uppercase"]
+            password += random_word(uppercase)
+        if json_data["special"]:
+            password += random.choice(string.punctuation)
+        if json_data["number"]:
+            password += random.choice(string.digits)
+        return password
 
 
 if __name__ == "__main__":
