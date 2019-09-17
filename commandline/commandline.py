a048ca114df9c31557e59326f964a31299cd8c1e
rick <rick@nbinvincible.org.uk>
2019-09-17 16:38:49 +0100

    Format strings may be changed

--- commandline/commandline.py
+++ commandline/commandline.py
@@ -50,17 +50,21 @@ def divide_chain(integers):
     return result
 
 
-parser = argparse.ArgumentParser(description="Process some integers.")
+parser = argparse.ArgumentParser(
+    description="Process some integers.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 parser.add_argument(
     "integers", metavar="N", type=int, nargs="+", help="an integer for the accumulator"
 )
+
 parser_operators = parser.add_argument_group("operators", "operations to carry out")
 parser_operators.add_argument(
     "-s",
     "--sum",
     dest="functions",
     action="append_const",
-    const=("sum", sum, "{}"),
+    const=("sum", sum, "int_fmt"),
     help="sum the integers",
 )
 parser_operators.add_argument(
@@ -68,7 +72,7 @@ parser_operators.add_argument(
     "--max",
     dest="functions",
     action="append_const",
-    const=("max", max, "{}"),
+    const=("max", max, "int_fmt"),
     help="find the maximum integer",
 )
 parser_operators.add_argument(
@@ -76,7 +80,7 @@ parser_operators.add_argument(
     "--prod",
     dest="functions",
     action="append_const",
-    const=("product", product, "{}"),
+    const=("product", product, "int_fmt"),
     help="find the product of the integers",
 )
 parser_operators.add_argument(
@@ -84,27 +88,52 @@ parser_operators.add_argument(
     "--div",
     dest="functions",
     action="append_const",
-    const=("div chain", divide_chain, "{:.5f}"),
+    const=("div chain", divide_chain, "float_fmt"),
     help="carry out a division chain on the integers",
+)
+
+parser_formats = parser.add_argument_group("formats", "number output format strings")
+parser_formats.add_argument(
+    "--int_fmt",
+    dest="int_fmt",
+    action="store",
+    default="{}",
+    help="format string for integers",
+)
+parser_formats.add_argument(
+    "--float_fmt",
+    dest="float_fmt",
+    action="store",
+    default="{:.5f}",
+    help="format string for floats",
 )
 
 args = parser.parse_args()
 
+# Get the content of args as a dict()
+argsd = vars(args)
+
+
 output = []
-intercepted_exception = None
+exceptions = []
 for functpl in args.functions:
     try:
         result = functpl[1](args.integers)
     except ZeroDivisionError as ex:
         result_str = "div by zero"
-        intercepted_exception = ex
+        exceptions.append(ex)
     except ValueError as ex:
         result_str = "arg to large"
-        intercepted_exception = ex
+        exceptions.append(ex)
     except Exception as ex:
-        raise ex
+        exceptions.append(ex)
     else:
-        result_str = functpl[2].format(result)
+        fmt_str = argsd[functpl[2]]
+        try:
+            result_str = fmt_str.format(result)
+        except Exception as ex:
+            result_str = "error"
+            exceptions.append(ex)
 
     output.append(f"{functpl[0]}: {result_str}")
 
@@ -113,20 +142,22 @@ print(", ".join(output))
 # Demonstrate some of the many cool things you can find out about an exception
 # This is 'Deep Magic', so feel free to ignore the details, or all of it
 
-# None evaluates to false, so this responds only if we have intercepted an exception.
-if intercepted_exception:
+# We have saved all the exceptions raised in individual operations, now we have shown all the
+# results we can output them
+for ex in exceptions:
 
     # 'Tracebacks' contain all the information about which parts of your code, and some libraries, were involved
     # in the exception
-    ex_tb = traceback.TracebackException.from_exception(intercepted_exception)
+    ex_tb = traceback.TracebackException.from_exception(ex)
 
     # A 'Frame' holds data about an individual part of the path through the code
     ex_frame = ex_tb.stack[-1]
     ex_origin = ex_frame.name
+    ex_line = ex_frame.lineno
 
     # 'dunderscore' variables expose some of the hidden workings of python
     # This gets the name of the exception type, without the formatting that Python helpfully provides
-    ex_type_ = type(intercepted_exception).__name__
+    ex_type_ = type(ex).__name__
 
     # Put all this together in an almost friendly error message for the user
-    print(f"... {ex_type_}: '{intercepted_exception}' in function {ex_origin}")
+    print(f"... {ex_type_}: {ex} in function {ex_origin} line {ex_line}")
